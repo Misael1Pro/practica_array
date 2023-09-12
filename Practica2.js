3fef4e8e2b94aeecb291408f6091281b9c7f4164
Misael <betancourthmisael2001@gmail.com>
2023-09-11 22:16:59 -0600

    Agregando comentario a las lineas de codigo

--- Practica2.js
+++ Practica2.js
@@ -1,7 +1,7 @@
- let myArray=["Pizza", "Marmaon","Pollo"]
-console.log(myArray)
-myArray[0]="Shup Suey"
-myArray[1]="Hamburguesas"
- console.log(myArray);
-(myArray.unshift("Baleadas"))
-console.log(myArray)
+ let myArray=["Pizza", "Marmaon","Pollo"] //Array con comidas favoritas
+console.log(myArray)   //Imprimiendo en pantalla
+myArray[0]="Shup Suey" //Sustituyendo array en indicie 0
+myArray[1]="Hamburguesas" //Sustituyendo array en indice 1
+ console.log(myArray);  //Imprimiendo nueva lista de comidas
+(myArray.unshift("Baleadas")) //Agregando una comida más a mi array
+console.log(myArray) //Mostrando nueva lista de comida
